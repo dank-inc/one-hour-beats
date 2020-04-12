a20e3ea88f35f68f960ad7d3f7b339acb960adc1
Elijah Lucian <elijahlucian@gmail.com>
2020-04-11 20:57:34 -0600

    bit of convenience

--- src/server.js
+++ src/server.js
@@ -1,25 +1,41 @@
 import sirv from "sirv";
 import polka from "polka";
+import send from "@polka/send-type";
+import _ from "lodash";
 import compression from "compression";
 import * as sapper from "@sapper/server";
+
+import jams from "./mock-db/jams";
+import entries from "./mock-db/entries";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-const app = polka()
-  .get("/api/jams/:id?", (req, res, next) => {
-    if (req.params.id) {
-      res.end(`getting jam: ${req.params.id}`);
+const jamIndex = _.keyBy(jams, "id");
+const entryByJam = {};
+for (const entry of entries) {
+  entryByJam[entry.jamId] = [...(entryByJam[entry.jamId] || []), entry];
+}
+
+const app = polka();
+app.store = { jamIndex, entryByJam };
+
+app
+  .get("/api/jams/:id?", ({ params }, res, next) => {
+    const { store } = app;
+    if (params.id) {
+      send(res, 200, store.jamIndex[params.id]);
     }
-    res.end(`getting all jams`);
+    send(res, 200, store.jamIndex);
   })
   // post to /jams => create jam
   // put to /jams/:id => update jam
-  .get("/api/entries/:id?", (req, res, next) => {
-    if (req.params.id) {
-      res.end(`getting jam: ${req.params.id}`);
+  .get("/api/entries/:jamId?/:id?", ({ params }, res, next) => {
+    const { store } = app;
+    if (params.jamId) {
+      send(res, 200, store.entryByJam[params.jamId]);
     }
-    res.end(`getting all jams`);
+    send(res, 200, store.entryByJam);
   })
   // post to /entries => create entry
   // put to /entries/:id => update entry *milestone 2
