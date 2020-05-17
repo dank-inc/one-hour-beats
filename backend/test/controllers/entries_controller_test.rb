db6acaa3d52a8ec5c5e03dd42dc18adb24d98f24
= <=>
2020-05-17 09:10:14 -0600

    trying to fix tests

--- backend/test/controllers/entries_controller_test.rb
+++ backend/test/controllers/entries_controller_test.rb
@@ -17,7 +17,7 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create entry" do
     assert_difference('Entry.count') do
-      post entries_url, params: { entry: { jam_id: @entry.jam_id, link: @entry.link, string: @entry.string, title: @entry.title, user_id: @entry.user_id } }
+      post entries_url, params: { entry: { jam_id: @entry.jam_id, link: @entry.link, title: @entry.title, user_id: @entry.user_id } }
     end
 
     assert_redirected_to entry_url(Entry.last)
@@ -34,7 +34,7 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update entry" do
-    patch entry_url(@entry), params: { entry: { jam_id: @entry.jam_id, link: @entry.link, string: @entry.string, title: @entry.title, user_id: @entry.user_id } }
+    patch entry_url(@entry), params: { entry: { jam_id: @entry.jam_id, link: @entry.link, title: @entry.title, user_id: @entry.user_id } }
     assert_redirected_to entry_url(@entry)
   end
 
