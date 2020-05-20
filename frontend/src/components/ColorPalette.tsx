577cfb551bf3bb51d2e6c6acceb70f621c3a9414
Jamie Schmitz <schmitzcj@protonmail.com>
2020-05-20 12:00:29 -0400

    Updates to preferences page, schema, db

---
+++ frontend/src/components/ColorPalette.tsx
@@ -0,0 +1,12 @@
+import * as colors from '@ant-design/colors'
+
+export const options = [
+  [0, colors.cyan[5], '#fff'],
+  [1, colors.magenta[6], '#fff'],
+  [2, colors.lime[6], '#fff'],
+  [3, colors.gold[6], '#fff'],
+  [4, colors.red[4], '#fff'],
+  [5, colors.blue[4], '#fff'],
+  [6, colors.purple[4], '#fff'],
+  [7, colors.volcano[5], '#fff'],
+]
