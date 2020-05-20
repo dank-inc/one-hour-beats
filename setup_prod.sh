39a0f9c3d5cd043bc5f0ab03c6a664b2d73969fb
= <=>
2020-05-20 14:59:06 -0600

    ci brouhaha

--- setup_prod.sh
+++ setup_prod.sh
@@ -1,3 +1,10 @@
+# deploy frontend
+
+docker-compose -f prod-frontend-build build
+docker-compose -f prod-frontend-build run --rm npm run build
+
+# deploy backend
+
 docker-compose -f prod-compose.yml build 
 docker-compose -f prod-compose.yml run --rm backend rails db:reset db:seed
 docker-compose -f prod-compose.yml up
