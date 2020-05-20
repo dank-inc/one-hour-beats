ce3d257451d00efe660f42430a4f6c60501e4ee3
= <=>
2020-05-20 17:26:38 -0600

    stuff

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,1 +1,1 @@
-cd one-hour-beats && docker-compose build --no-cache frontend && docker-compose -f prod-frontend-deploy.yml run --rm frontend npm run build
+cd ~/one-hour-beats && docker-compose build --no-cache frontend && docker-compose -f prod-frontend-deploy.yml run --rm frontend npm run build
