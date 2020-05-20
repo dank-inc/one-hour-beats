22b3164611afb7913a4b10e19b06850d332db38f
= <=>
2020-05-20 16:40:49 -0600

    stuff

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,1 +1,1 @@
-git pull && docker-compose -f prod-compose.yml down && docker-compose -f prod-compose.yml build && docker-compose -f prod-compose.yml run --rm backend rails db:reset db:seed && docker-compose -f prod-compose.yml up -d
+cd one-hour-beats && git pull && docker-compose -f prod-compose.yml down && docker-compose -f prod-compose.yml build && docker-compose -f prod-compose.yml run --rm backend rails db:reset db:seed && docker-compose -f prod-compose.yml up -d
---
+++ deploy-frontend-from-local.sh
@@ -0,0 +1,1 @@
+cd frontend && npm i && npm run build && scp -vr build/* elijahlucian@onehourbeats.com:/var/www/html/
