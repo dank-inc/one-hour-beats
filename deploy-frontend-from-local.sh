2b42dbfcd1b96ba9803404cceaffb456d95950d5
= <=>
2020-08-09 17:12:11 -0600

    tweaks

--- deploy-frontend-from-local.sh
+++ deploy-frontend-from-local.sh
@@ -1,3 +1,5 @@
+# TODO rsync
+
 cd frontend && npm i && npm run build && scp -vr build/* root@onehourbeats.com:/var/www/html/
 
 cd frontend; npm i; npm run build; scp -vr build/* root@onehourbeats.com:/var/www/html/
--- frontend/src/routes/jams/index.tsx
+++ frontend/src/routes/jams/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { PageHeader } from 'antd'
+import { PageHeader, Empty, Button } from 'antd'
 import './style.scss'
 import { JamCard } from 'components/JamCard'
 import moment from 'moment'
 import { useUserContext } from 'contexts/UserContext'
 import { useAppContext } from 'contexts/AppContext'
+import { RouteChildrenProps } from 'react-router'
 
 window.moment = moment
 
-export const Jams = () => {
+export const Jams = ({ history }: RouteChildrenProps) => {
   const { user } = useUserContext()
   const { jamIndex, jamRoomUsers } = useAppContext()
 
@@ -20,13 +21,21 @@ export const Jams = () => {
         subTitle={`Welcome ${user.name}, Pick a challenge and get jammin!`}
       />
       <div className="main-content jam-cards">
-        {Object.values(jamIndex).map((jam) => (
-          <JamCard
-            jam={jam}
-            key={`jam-list-${jam.id}-${jam.user_id}`}
-            users={jamRoomUsers[jam.id]}
-          />
-        ))}
+        {Object.values(jamIndex)?.length ? (
+          Object.values(jamIndex).map((jam) => (
+            <JamCard
+              jam={jam}
+              key={`jam-list-${jam.id}-${jam.user_id}`}
+              users={jamRoomUsers[jam.id]}
+            />
+          ))
+        ) : (
+          <Empty description="No Jams here!">
+            <Button onClick={() => history.push('/create')}>
+              Go Make One!
+            </Button>
+          </Empty>
+        )}
       </div>
     </main>
   )
