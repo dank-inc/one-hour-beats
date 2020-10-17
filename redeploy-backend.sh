682da29ff4c115005dcd0d6ae4ae5b60a76cbfef
Elijah Lucian <elijahlucian@gmail.com>
2020-10-17 15:12:48 -0600

    hotfix

--- redeploy-backend.sh
+++ redeploy-backend.sh
@@ -1,1 +1,6 @@
-cd ~/one-hour-beats && git pull && docker-compose -f prod-compose.yml down && docker-compose -f prod-compose.yml build && docker-compose -f prod-compose.yml up -d
+# git pull && \
+docker-compose -f prod-compose.yml down && \
+docker-compose -f prod-compose.yml build && \
+docker-compose -f prod-compose.yml run --rm backend rails db:migrate && \
+docker-compose -f prod-compose.yml down && \
+docker-compose -f prod-compose.yml up -d
